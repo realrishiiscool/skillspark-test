fcbf322fdbd945f4b80419e5af033b99197eb963
realrishiiscool <2108120.rishivardhan@gmail.com>
2026-03-05 19:40:27 +0530

    working 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Index from "./pages/Index";
 import SelectTechnology from "./pages/SelectTechnology";
@@ -17,15 +18,18 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <HashRouter>
         <Navbar />
+
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/select" element={<SelectTechnology />} />
           <Route path="/test" element={<TestPage />} />
-          <Rout e path="/results" element={<ResultsPage />} />
+          <Route path="/results" element={<ResultsPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+
       </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
